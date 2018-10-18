00562df2133a48e975ce167d4081f34c60184ff9
Jerson Martínez <jersonmartinezsm@gmail.com>
2018-10-17 21:41:29 -0600

    Last updates about shell script of the VM gnet_web and gnet_db

--- Vagrant-GNet/ProvisionScripts/db.sh
+++ Vagrant-GNet/ProvisionScripts/db.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+#COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+
+export DEBIAN_FRONTEND="noninteractive"
+
 # Configuraciones
 DBHOST=10.0.100.102
 HOSTWEB=10.0.100.101
@@ -8,38 +21,39 @@ DBUSER=root
 DBPASSWD=root
 
 function UpdateHost(){
-    echo -e "\n--- {Actualizando la lista de paquetes y el sistema} ---\n"
-    sudo apt-get update && sudo apt-get upgrade
+    echo -e "$Cyan \n--- {Actualizando la lista de paquetes y el sistema} ---\n $Color_Off"
+    sudo apt-get -y update >> /var/log/vm_build.log 2>&1
+    sudo apt-get -y upgrade >> /var/log/vm_build.log 2>&1
 }
 
 function BasePackages(){
-    echo -e "\n--- {Instalando paquetes base [vim y git]} ---\n"
-    sudo apt-get install -y vim git >> /var/log/vm_build.log 2>&1
+    echo -e "$Cyan \n--- {Instalando paquetes base [vim, git y debconf-utils]} ---\n $Color_Off"
+    sudo apt-get install -y vim git debconf-utils >> /var/log/vm_build.log 2>&1
 }
 
 function InstallMySQL(){
-    echo -e "\n--- {Instalando MySQL [Iniciando las configuraciones entrantes sobre las credenciales del SGDB]} ---\n"
+    echo -e "$Cyan \n--- {Instalando MySQL [Iniciando las configuraciones entrantes sobre las credenciales del SGDB]} ---\n $Color_Off"
     # Iniciando las configuraciones entrantes sobre las credenciales del SGDB
     debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
     debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
 
-    echo -e "\n--- {Instalando MySQL [MySQL Server]} ---\n"
+    echo -e "$Cyan \n--- {Instalando MySQL [MySQL Server]} ---\n $Color_Off"
     # Instalando el SGDB MySQL Server
     sudo apt-get install -y mysql-server >> /var/log/vm_build.log 2>&1
 }
 
 function ConfigureMySQL(){
-    echo -e "\n--- {Configurando MySQL [Crear base de datos y usuario con máximos privilegios]} ---\n"
+    echo -e "$Cyan \n--- {Configurando MySQL [Crear base de datos y usuario con máximos privilegios]} ---\n $Color_Off"
     # Instalando el SGDB MySQL Server
     mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME" >> /var/log/vm_build.log 2>&1
     mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'$DBHOST' identified by '$DBPASSWD'" > /var/log/vm_build.log 2>&1
     mysql -uroot -p$DBPASSWD -e "grant all privileges on *.* to 'root'@'$HOSTWEB' identified by '$DBPASSWD'" > /var/log/vm_build.log 2>&1
 
-    echo -e "\n--- {Configurando MySQL [Cambiando la dirección de host]} ---\n"
+    echo -e "$Cyan \n--- {Configurando MySQL [Cambiando la dirección de host]} ---\n $Color_Off"
     # Configurando la dirección de host
     sed -i "s/127.0.0.1/$DBHOST/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
-    echo -e "\n--- {Configurando MySQL [Reiniciando el servicio]} ---\n"
+    echo -e "$Cyan \n--- {Configurando MySQL [Reiniciando el servicio]} ---\n $Color_Off"
     # Reiniciando el servicio MySQL
     systemctl restart mysql.service
 }
--- Vagrant-GNet/ProvisionScripts/web.sh
+++ Vagrant-GNet/ProvisionScripts/web.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+#COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+
+export DEBIAN_FRONTEND="noninteractive"
+
 # Ruta absoluta donde se agregará el proyecto.
-DirStorage="/var/www/html/GNet"
+DirStorage="/var/www/html"
 
 # Ruta del directorio compartido
 DirShared="/vagrant/GNet"
@@ -13,90 +26,106 @@ DBUSER=root
 DBPASSWD=root
 
 #Crea los directorios que conforman la ruta $DirStorage
-function CreateDirs(){
-	# Si la ruta no existe, la crea, directorios y subdirectorios
-	[ ! -d $DirStorage ] && mkdir -p ${DirStorage}
-
-    # Se habilita un enlace simbólico del proyecto
+function LinkDirs(){
+    echo -e "$Cyan \n--- {Asignando permisos y creando un enlace simbólico} ---\n $Color_Off"
+    # Asignando permisos y creando un enlace simbólico
+    chmod 0777 -R -f $DirShared
     ln -fs $DirShared $DirStorage
-
-	# Se asignan permisos recursivo 777 a $PathAdsolute | Silencioso -f
-	chmod 0777 -R -f $(dirname $(dirname $DirStorage))
 }
 
 function UpdateHost(){
-    echo -e "\n--- {Actualizando la lista de paquetes y el sistema} ---\n"
-    sudo apt-get update && sudo apt-get upgrade >> /var/log/vm_build.log 2>&1
+    echo -e "$Cyan \n--- {Actualizando la lista de paquetes y el sistema} ---\n $Color_Off"
+    sudo apt-get -y update >> /var/log/vm_build.log 2>&1
+    sudo apt-get -y upgrade >> /var/log/vm_build.log 2>&1
 }
 
 function BasePackages(){
-    echo -e "\n--- {Instalando paquetes base [vim y git]} ---\n"
-    sudo apt-get install -y vim git >> /var/log/vm_build.log 2>&1
+    echo -e "$Cyan \n--- {Instalando paquetes base [vim, git y debconf-utils]} ---\n $Color_Off"
+    sudo apt-get install -y vim git debconf-utils >> /var/log/vm_build.log 2>&1
 }
 
 function InstallWebServer(){
-    echo -e "\n--- {Instalando el servicio web [Apache]} ---\n"
+    echo -e "$Cyan \n--- {Instalando el servicio web [Apache]} ---\n $Color_Off"
     sudo apt-get install -y apache2 >> /var/log/vm_build.log 2>&1
+
+    InstallFirewall
+}
+
+function InstallFirewall(){
+    echo -e "$Cyan \n--- {Instalando Firewall [UFW]} ---\n $Color_Off"
+    # Instalando Firewall
+    sudo apt-get install -y ufw
+
+    # Permitiendo el tráfico
+    sudo ufw allow http
+    sudo ufw allow https
 }
 
 function InstallPHP(){
-    echo -e "\n--- {Instalando PHP [add-apt-repository ppa:ondrej/php]} ---\n"
+    echo -e "$Cyan \n--- {Instalando PHP [add-apt-repository ppa:ondrej/php]} ---\n $Color_Off"
     # Agregando a la lista de paquetes
-    sudo add-apt-repository ppa:ondrej/php -y
+    sudo add-apt-repository ppa:ondrej/php -y >> /var/log/vm_build.log 2>&1
     
-    echo -e "\n--- {Repitiendo esta acción} ---\n"
+    echo -e "$Cyan \n--- {Repitiendo esta acción} ---\n $Color_Off"
     # Actualizar lista de paquetes
     UpdateHost
     
-    echo -e "\n--- {Instalando PHP [php7.0, libapache2-mod-php7.0]} ---\n"
+    echo -e "$Cyan \n--- {Instalando PHP [php7.0, libapache2-mod-php7.0]} ---\n $Color_Off"
     # Instalación de PHP y el módulo Apache
     sudo apt-get install -y php7.0 \
-        libapache2-mod-php7.0 >> /var/log/vm_build.log 2>&1
+        libapache2-mod-php7.0 \
+        php-pear >> /var/log/vm_build.log 2>&1
 
-    echo -e "\n--- {Instalando PHP [Buscando en la caché]} ---\n"
+    echo -e "$Cyan \n--- {Instalando PHP [Buscando en la caché]} ---\n $Color_Off"
     # Busca en la caché los paquetes de PHP en la versión 7.0
     sudo apt-cache search php7.0 >> /var/log/vm_build.log 2>&1
     
-    echo -e "\n--- {Instalando PHP [php7.0-{packages}]} ---\n"
+    echo -e "$Cyan \n--- {Instalando PHP [php7.0-{packages}]} ---\n $Color_Off"
     # Instala los conectores al SGDB y otras librerías
-    sudo apt-get install -y php7.0-{mysqli,mysql,curl,gd,intl,imagick,imap,mcrypt,memcache,pspell,recode,sqlite3,tidy,xmlrpc,xsl,mbstring,gettext} >> /var/log/vm_build.log 2>&1
+    sudo apt-get install -y php7.0-{mysqli,mysql,curl,gd,intl,imagick,imap,mcrypt,memcache,pspell,recode,sqlite3,tidy,xmlrpc,xsl,mbstring,gettext,json,cgi} >> /var/log/vm_build.log 2>&1
     
-    echo -e "\n--- {Instalando PHP [php-ssh2} ---\n"
+    echo -e "$Cyan \n--- {Instalando PHP [php-ssh2} ---\n $Color_Off"
     # Instala la librería SSH2
     sudo apt-get install -y php-ssh2 >> /var/log/vm_build.log 2>&1
 }
 
 function InstallPHPMyAdmin(){
-    echo -e "\n--- {Instalando PHP [PHPMyAdmi - Configurando entrada sobre las credenciales]} ---\n"
-    # Configura las credenciales que pide en paquete
-    debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-    debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD"
-    debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD"
-    debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
-    debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
-
-    echo -e "\n--- {Instalando PHP [Instalando PHPMyAdmin]} ---\n"
+    echo -e "$Cyan \n--- {Instalando PHP [Instalando PHPMyAdmin]} ---\n $Color_Off"
     # Instala PHPMyAdmin
-    sudo apt-get install -y phpmyadmin >> /var/log/vm_build.log 2>&1
+    sudo apt-get install -yq phpmyadmin
+    
+    echo -e "$Cyan \n--- {Instalando PHP [PHPMyAdmin - Configurando entrada sobre las credenciales]} ---\n $Color_Off"
+    # Configura las credenciales que pide en paquete
+    sudo dpkg-reconfigure --frontend=noninteractive phpmyadmin
+    sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+    sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD"
+    sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD"
+    sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
+    sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
 }
 
 function ConfigurePHP(){
-    echo -e "\n--- {Configurando PHP [a2enmod rewrite]} ---\n"
+    echo -e "$Cyan \n--- {Configurando PHP [a2enmod rewrite]} ---\n $Color_Off"
     # Habilitando mod-rewrite
     a2enmod rewrite >> /var/log/vm_build.log 2>&1
 
-    echo -e "\n--- {Configurando PHP [Apache Override All]} ---\n"
+    echo -e "$Cyan \n--- {Configurando PHP [Apache Override All]} ---\n $Color_Off"
     # Permitiendo a Apache Override All
     sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
 
-    echo -e "\n--- {Configurando PHP [Reiniciando Apache]} ---\n"
+    # Permisos
+    echo -e "$Cyan \n--- Permiso para /var/www ---\n $Color_Off"
+    sudo chown -R www-data:www-data /var/www
+    echo -e "$Green \n--- Permisos establecidos ---\n $Color_Off"
+
+    echo -e "$Cyan \n--- {Configurando PHP [Reiniciando Apache]} ---\n $Color_Off"
     # Reiniciar Apache
-    service apache2 restart >> /var/log/vm_build.log 2>&1
+    sudo service apache2 restart
 }
 
 UpdateHost
 BasePackages
 InstallWebServer
-CreateDirs
+LinkDirs
 InstallPHP
 InstallPHPMyAdmin
