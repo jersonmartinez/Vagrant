8022fd6ce8b48b1e2f714461ec91a610d477d594
Jerson Martinez <jersonmartinezsm@gmail.com>
2018-11-10 12:30:16 -0600

    Fix dedconf and refactoring code Vagrant provision

--- Vagrant-GNet/ProvisionScripts/web.sh
+++ Vagrant-GNet/ProvisionScripts/web.sh
@@ -32,26 +32,26 @@ function LinkDirs(){
 }
 
 function UpdateHost(){
-    echo -e "$Cyan \n--- {Actualizando la lista de paquetes y el sistema} ---\n $Color_Off"
+    echo -e "$Cyan \n--- {Actualizando la lista de paquetes en el sistema} ---\n $Color_Off"
     sudo apt-get -y update >> /var/log/vm_build.log 2>&1
     sudo apt-get -y upgrade >> /var/log/vm_build.log 2>&1
 }
 
 function BasePackages(){
-    echo -e "$Cyan \n--- {Instalando paquetes base [vim, git y debconf-utils]} ---\n $Color_Off"
+    echo -e "$Cyan \n--- {Paquetes base [Instalando: vim, git y debconf-utils]} ---\n $Color_Off"
     sudo apt-get install -y git debconf-utils build-essential binutils-doc >> /var/log/vm_build.log 2>&1
 }
 
 function InstallWebServer(){
-    echo -e "$Cyan \n--- {Instalando el servicio web [Apache]} ---\n $Color_Off"
+    echo -e "$Cyan \n--- {Apache [Instalando servicio]} ---\n $Color_Off"
     sudo apt-get install -y apache2 >> /var/log/vm_build.log 2>&1
 
     InstallFirewall
 }
 
 function InstallFirewall(){
-    echo -e "$Cyan \n--- {Instalando Firewall [UFW]} ---\n $Color_Off"
     # Instalando Firewall
+    echo -e "$Cyan \n--- {Instalando Firewall [UFW]} ---\n $Color_Off"
     sudo apt-get install -y ufw >> /var/log/vm_build.log 2>&1
 
     # Permitiendo el tráfico
@@ -60,83 +60,91 @@ function InstallFirewall(){
 }
 
 function InstallPHP(){
-    echo -e "$Cyan \n--- {Instalando PHP [add-apt-repository ppa:ondrej/php]} ---\n $Color_Off"
     # Agregando a la lista de paquetes
+    echo -e "$Cyan \n--- {PHP [add-apt-repository ppa:ondrej/php]} ---\n $Color_Off"
     sudo add-apt-repository ppa:ondrej/php -y >> /var/log/vm_build.log 2>&1
     
-    echo -e "$Cyan \n--- {Repitiendo esta acción} ---\n $Color_Off"
     # Actualizar lista de paquetes
     UpdateHost
     
-    echo -e "$Cyan \n--- {Instalando PHP [php7.0, libapache2-mod-php7.0]} ---\n $Color_Off"
     # Instalación de PHP y el módulo Apache
+    echo -e "$Cyan \n--- {PHP [Instalando PHP 7.0]} ---\n $Color_Off"
     sudo apt-get install -y php7.0 \
         libapache2-mod-php7.0 \
         php-pear >> /var/log/vm_build.log 2>&1
 
-    echo -e "$Cyan \n--- {Instalando PHP [Buscando en la caché]} ---\n $Color_Off"
     # Busca en la caché los paquetes de PHP en la versión 7.0
+    echo -e "$Cyan \n--- {PHP [Buscando en la caché]} ---\n $Color_Off"
     sudo apt-cache search php7.0 >> /var/log/vm_build.log 2>&1
     
-    echo -e "$Cyan \n--- {Instalando PHP [php7.0-{packages}]} ---\n $Color_Off"
     # Instala los conectores al SGDB y otras librerías
+    echo -e "$Cyan \n--- {PHP [Instalando extensiones php7.0-{packages}]} ---\n $Color_Off"
     sudo apt-get install -y php7.0-{mysqli,mysql,curl,gd,intl,imagick,imap,mcrypt,memcache,pspell,recode,sqlite3,tidy,xmlrpc,xsl,mbstring,gettext,json,cgi} >> /var/log/vm_build.log 2>&1
     
-    echo -e "$Cyan \n--- {Instalando PHP [php-ssh2} ---\n $Color_Off"
     # Instala la librería SSH2
+    echo -e "$Cyan \n--- {PHP [Instalando extensión importante: php-ssh2} ---\n $Color_Off"
     sudo apt-get install -y php-ssh2 >> /var/log/vm_build.log 2>&1
 }
 
 function ConfigurePHP(){
-    echo -e "$Cyan \n--- {Configurando PHP [a2enmod rewrite]} ---\n $Color_Off"
     # Habilitando mod-rewrite
+    echo -e "$Cyan \n--- {PHP [Configurando: a2enmod rewrite]} ---\n $Color_Off"
     sudo a2enmod rewrite >> /var/log/vm_build.log 2>&1
 
-    echo -e "$Cyan \n--- {Configurando PHP [Apache Override All]} ---\n $Color_Off"
     # Permitiendo a Apache Override All
+    echo -e "$Cyan \n--- {PHP [Configurando: Apache Override All]} ---\n $Color_Off"
     sudo sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
 
-    # Permisos
-    echo -e "$Cyan \n--- Permiso para /var/www ---\n $Color_Off"
+    # Asignando permisos
+    echo -e "$Cyan \n--- Estableciendo permisos para /var/www ---\n $Color_Off"
     sudo chown -R www-data:www-data /var/www/html/
-    echo -e "$Green \n--- Permisos establecidos ---\n $Color_Off"
 
-    echo -e "$Cyan \n--- {Configurando PHP [Reiniciando Apache]} ---\n $Color_Off"
     # Reiniciar Apache
+    echo -e "$Cyan \n--- {PHP [Reiniciando Apache]} ---\n $Color_Off"
     sudo service apache2 restart
 }
 
 function InstallPHPMyAdmin(){
+    #Limpiar las configuraciones desatendidas sobre la base de datos
+    echo -e "$Cyan \n--- {Debconf [FixDB para aplicar instalación desatendida]} ---\n $Color_Off"
+    sudo /usr/share/debconf/fix_db.pl
+
+    # Configurar las credenciales e instalar MySQL Client y Server
+    echo -e "$Cyan \n--- {Instalando MySQL [Client & Server para PHPMyAdmin]} ---\n $Color_Off"
+    echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+    sudo apt-get install mysql-client mysql-server -y >> /var/log/vm_build.log 2>&1
+
     # Configura las credenciales que pide en paquete
-    echo -e "$Cyan \n--- {Instalando PHP [PHPMyAdmin - Configurando entrada sobre las credenciales]} ---\n $Color_Off"
-    echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
-    echo 'phpmyadmin phpmyadmin/app-password-confirm password $DBPassword' | debconf-set-selections
-    echo 'phpmyadmin phpmyadmin/mysql/admin-pass password $DBPassword' | debconf-set-selections
-    echo 'phpmyadmin phpmyadmin/mysql/app-pass password $DBPassword' | debconf-set-selections
-    echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect $WebServer' | debconf-set-selections
+    echo -e "$Cyan \n--- {PHPMyAdmin desatendido [Credenciales -> DBPass: $DBPassword, WebServer: $WebServer]} ---\n $Color_Off"
+    echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | sudo debconf-set-selections
+    echo 'phpmyadmin phpmyadmin/app-password-confirm password $DBPassword' | sudo debconf-set-selections
+    echo 'phpmyadmin phpmyadmin/mysql/admin-pass password $DBPassword' | sudo debconf-set-selections
+    echo 'phpmyadmin phpmyadmin/mysql/app-pass password $DBPassword' | sudo debconf-set-selections
+    echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect $WebServer' | sudo debconf-set-selections
     
     # Instala PHPMyAdmin
-    echo -e "$Cyan \n--- {Instalando PHP [Instalando PHPMyAdmin]} ---\n $Color_Off"
-    apt-get install -y phpmyadmin >> /var/log/vm_build.log 2>&1
+    echo -e "$Cyan \n--- {PHPMyAdmin [Instalación desatendida]} ---\n $Color_Off"
+    sudo apt-get install -y phpmyadmin >> /var/log/vm_build.log 2>&1
 
-    echo -e "$Cyan \n--- {Configurando PHPMyAdmin [Asignando el servidor remoto: $DBIPHost]} ---\n $Color_Off"
-    # sed -i 's/\$cfg\['Servers'\]\[\$i\]\['host'\] =/\$cfg\['Servers'\]\[\$i\]\['host'\] = \$dbserver/g' /etc/phpmyadmin/config.inc.php
+    echo -e "$Cyan \n--- {PHPMyAdmin [Configurando ervidor remoto de base de datos: $DBIPHost]} ---\n $Color_Off"
+    sudo sed -i.bak -e "s/\$cfg\['Servers'\]\[\$i\]\['host'\] = \$dbserver;/\$cfg['Servers'][\$i]['host'] = '$DBIPHost';/" /etc/phpmyadmin/config.inc.php
 }
 
 function InstallNMap(){
-    echo -e "$Cyan \n--- {Instalando NMap [Network Mapper]} ---\n $Color_Off"
+    echo -e "$Cyan \n--- {NMap [Instalación - Network Mapper]} ---\n $Color_Off"
     sudo apt-get install -y nmap >> /var/log/vm_build.log 2>&1
 }
 
 function ConfigSSH(){
-    echo -e "$Cyan \n--- {Configurando SSH Server [Habilitando las directivas: PasswordAuthentication, PermitRootLogin]} ---\n $Color_Off"
+    echo -e "$Cyan \n--- {SSH Server [Habilitando las directivas: PasswordAuthentication, PermitRootLogin]} ---\n $Color_Off"
     sed -i 's/PasswordAuthentication/#PasswordAuthentication/g' /etc/ssh/sshd_config
     sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
     service ssh restart
 }
 
 function AssignUserPassword(){
-    echo -e "$Cyan \n--- {Asignando contraseña a un usuario [Credenciales-> Username: $2, Password: $1]} ---\n $Color_Off"
+    echo -e "$Cyan \n--- {Asignando contraseña al usuario [Credenciales-> Username: $2, Password: $1]} ---\n $Color_Off"
     echo -e "$1\n$1\n" | sudo passwd $2 >> /var/log/vm_build.log 2>&1
 }
 
